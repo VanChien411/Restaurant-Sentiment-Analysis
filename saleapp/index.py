0b6829ffa7622fb86b9a7a711c0589394325a5e5
VanChien411 <96508741+VanChien411@users.noreply.github.com>
2024-04-03 20:12:50 +0700

    Update index.py

--- saleapp/index.py
+++ saleapp/index.py
@@ -85,4 +85,4 @@ def sentiment():
     return render_template('sentiment.html', confi=confi, label=label, post=post.replace("_", " "), pos=pos, neg=neg)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
